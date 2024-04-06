0741e2ef2ad86a9093c2275fc50741d6e01f29c5
JuniorDeveloper17 <ryanxxjr@gmail.com>
2024-04-06 19:25:48 +0700

    push projek

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-'user strict';
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const cors = require('cors');
-const corsConfig = {
-    origin: "*",
-    credential: true,
-    methods: ["GET", "POST", "OPTIONS"],
-}
-app.options("", cors(corsConfig))
-app.use(cors(corsConfig));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 const routes = require('./routes');
 routes(app);
